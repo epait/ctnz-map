aec875c39ba74b2f8bc3a9b36f48bf7c8b4917ee
Eric Pait <ericpait@gmail.com>
2016-01-13 17:08:42 -0500

    update from kmz to kml

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 6,
-    center: {lat: -41, lng: 174}
-  });
+var map;
+var src = '../assets/maps/full-trail.kml';
 
-  var ctaLayer = new google.maps.KmlLayer({
-    url: 'http://ericpait.com/te-araroa-trail-map.kmz',
-    map: map,
-    suppressInfoWindows: true
+/**
+ * Initializes the map and calls the function that creates polylines.
+ */
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: new google.maps.LatLng(-41, 174),
+    zoom: 6,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  });
+  loadKmlLayer(src, map);
+}
+
+/**
+ * Adds a KMLLayer based on the URL passed. 
+ */
+function loadKmlLayer(src, map) {
+  var kmlLayer = new google.maps.KmlLayer(src, {
+    suppressInfoWindows: true,
+    preserveViewport: false,
+    map: map
   });
 }

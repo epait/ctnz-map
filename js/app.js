31557b88b5a960ccb0f2b2252566d2d70146c3f1
Eric Pait <ericpait@gmail.com>
2016-01-21 12:18:58 -0500

    update route and map colors

--- js/app.js
+++ js/app.js
@@ -1,3 +1,46 @@
+var rootURL = 'http://www.ericpait.com/clients/nz-map/';
+
+var styles = [
+  {
+    featureType: "water",
+    stylers: [
+      { color: '#BAE8F5' }
+    ]
+  },
+  {
+  	featureType: "landscape",
+  	stylers: [
+  		{ color: "#f2f2f2" }
+  	]
+  },
+  {
+  	featureType: "poi",
+  	stylers: [
+  		{ color: "#f2f2f2" }
+  	]
+  },
+  {
+  	featureType: "road",
+  	stylers: [
+  		{ color: "#cccccc" }
+  	]
+  },
+  {
+    featureType: "road",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  },
+  {
+    featureType: "administrative.neighborhood",
+    elementType: "labels",
+    stylers: [
+      { visibility: "off" }
+    ]
+  }
+];
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 6,
@@ -6,9 +49,12 @@ function initMap() {
   console.log('map loaded');
 
   var ctaLayer = new google.maps.KmlLayer({
-    url: 'http://www.ericpait.com/clients/nz-map/assets/maps/full-trail.kmz',
+    url: rootURL + 'assets/maps/whanganui.kmz',
     suppressInfoWindows: true,
     map: map
   });
   console.log('kml loaded');
+
+  map.setOptions({styles: styles});
+  console.log('map styles loaded');
 }
